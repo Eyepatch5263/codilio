e7c19073370871708e2982264196aa67967279d4
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-01-28 08:20:56 +0000

    added terraform for digital_ocean

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+resource "random_id" "server" {
+	byte_length = 4
+}
+
+resource "digitalocean_ssh_key" "terraform-ssh-key" {
+	name = "terra-key"
+	public_key = file("/root/.ssh/terra.pub")
+}
+
+resource "digitalocean_droplet" "web" {
+	image = var.image
+	name = "terraform-${random_id.server.hex}"
+	region = "nyc1"
+	size = "s-2vcpu-4gb-120gb-intel"
+	ssh_keys = [digitalocean_ssh_key.terraform-ssh-key.fingerprint]
+}
+
+resource "digitalocean_firewall" "web" {
+  name = "only-22-80-and-443"
+
+  droplet_ids = [digitalocean_droplet.web.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8080"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
---
+++ terraform/varibale.tf
@@ -0,0 +1,10 @@
+variable "do_token" {
+	description = "Token"
+	type = string
+}
+
+variable "image" {
+	description = "Linux distro"
+	type = string
+	default = "ubuntu-24-04-x64"
+}
