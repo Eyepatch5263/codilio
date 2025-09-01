67845def063a46234422530c9d3f1630c8939730
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-09-02 01:37:41 +0530

    added speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { ClerkProvider } from "@clerk/nextjs";
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider";
@@ -45,6 +46,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-gradient-to-b from-gray-900 to-gray-950 text-gray-100 flex flex-col `}
         >
           <Analytics />
+          <SpeedInsights/>
           <ConvexClientProvider>
             {children}
           </ConvexClientProvider>
